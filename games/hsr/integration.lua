b065e47f86f3bab753d1a2b6bd4c8c970a533c3b
Nikita Podvirnyi <krypt0nn@vk.com>
2024-11-02 21:53:52 +0200

    feat(games/hsr): initial diff pipeline implementation

--- games/hsr/integration.lua
+++ games/hsr/integration.lua
@@ -44,6 +44,170 @@ return {
         end,
 
         get_diff = function(edition: string)
+            local iter = import("iterable")
+            local hyvlib = import("hyvlib").hyvlib
+
+            if not hyvlib.hsr[edition] then
+                error(`invalid edition: {edition}`)
+            end
+
+            local game = hyvlib.hsr[edition]
+
+            local api = game.api.get()
+            local result, current = pcall(game.parse_version)
+
+            if not result or not current then
+                return {
+                    title = {
+                        en = "Install game",
+                        ru = "Установить игру"
+                    },
+
+                    description = {
+                        en = "You don't have this game installed",
+                        ru = "У вас не установлена эта игра"
+                    },
+
+                    pipeline = {
+                        -- Download
+                        {
+                            title = {
+                                en = "Download",
+                                ru = "Скачать"
+                            },
+
+                            description = {
+                                en = "Download archives with game files",
+                                ru = "Скачать архивы с файлами игры"
+                            },
+
+                            perform = function(updater)
+                                local temp_folder = path.temp_dir()
+
+                                local expected_total = iter(api.segments)
+                                    .map(function(segment) return segment.download_size end)
+                                    .sum()
+
+                                local downloaded_total = 0
+
+                                for _, segment in iter(api.segments) do
+                                    local segment_name = path.file_name(segment.url)
+
+                                    downloader.download(segment.url, {
+                                        output_file = path.join(temp_folder, segment_name),
+                                        continue_downloading = true,
+
+                                        progress = function(current, total, diff)
+                                            downloaded_total += diff
+
+                                            updater({
+                                                title = {
+                                                    en = `Downloading {segment_name}`,
+                                                    ru = `Скачивается {segment_name}`
+                                                },
+
+                                                progress = {
+                                                    current = downloaded_total,
+                                                    total = math.max(expected_total, downloaded_total),
+
+                                                    format = function()
+                                                        local current = downloaded_total / 1000 / 1000 / 1000
+                                                        local total = math.max(expected_total, downloaded_total) / 1000 / 1000 / 1000
+
+                                                        current = math.floor(current * 100) / 100
+                                                        total = math.floor(total * 100) / 100
+
+                                                        return {
+                                                            en = `Downloaded {current} GB / {total} GB`,
+                                                            ru = `Скачано {current} ГБ / {total} ГБ`
+                                                        }
+                                                    end
+                                                }
+                                            })
+                                        end
+                                    })
+                                end
+                            end
+                        },
+
+                        -- Extract
+                        {
+                            title = {
+                                en = "Extract",
+                                ru = "Распаковать"
+                            },
+
+                            description = {
+                                en = "Extract game files from archives",
+                                ru = "Распаковать файлы игры из архивов"
+                            },
+
+                            perform = function(updater)
+                                local temp_folder = path.temp_dir()
+
+                                local archives = iter(api.segments)
+                                    .map(function(segment)
+                                        local segment_name = path.file_name(segment.url)
+                                        local segment_path = path.join(temp_folder, segment_name)
+
+                                        local handle = archive.open(segment_path)
+
+                                        return {
+                                            name   = segment_name,
+                                            handle = handle
+                                        }
+                                    end)
+                                    .collect()
+
+                                local expected_total = iter(archives)
+                                    .map(function(info)
+                                        return iter(archive.entries(info.handle))
+                                            .map(function(entry) return entry.size end)
+                                            .sum()
+                                    end)
+                                    .sum()
+
+                                local downloaded_total = 0
+
+                                for _, info in iter(archives) do
+                                    archive.extract(info.handle, game.paths.base_folder, function(current, total, diff)
+                                        downloaded_total += diff
+
+                                        updater({
+                                            title = {
+                                                en = `Extracting {info.name}`,
+                                                ru = `Распаковывается {info.name}`
+                                            },
+
+                                            progress = {
+                                                current = downloaded_total,
+                                                total = math.max(expected_total, downloaded_total),
+
+                                                format = function()
+                                                    local current = downloaded_total / 1000 / 1000 / 1000
+                                                    local total = math.max(expected_total, downloaded_total) / 1000 / 1000 / 1000
+
+                                                    current = math.floor(current * 100) / 100
+                                                    total = math.floor(total * 100) / 100
+
+                                                    return {
+                                                        en = `Extracted {current} GB / {total} GB`,
+                                                        ru = `Распаковано {current} ГБ / {total} ГБ`
+                                                    }
+                                                end
+                                            }
+                                        })
+                                    end)
+
+                                    archive.close(info.handle)
+                                end
+                            end
+                        }
+                    }
+                }
+            end
+
+            -- TODO
             return nil
         end,
 
